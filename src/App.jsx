a5b26f023e76a502898be1f7f884edbc563c1602
vimal004 <2004.vimal@gmail.com>
2024-07-27 10:40:23 +0530

    2

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Button } from "@mui/material";
 const App = () => {
   return (
     <div>
-      <h1 className="text-red-600">My React App</h1>
+      <h1 className="">My React App</h1>
       <Button>Click Me</Button> 
     </div>
   );
