6003d87fc97facab36e4d284470a4e5f68cb0d91
vimal004 <2004.vimal@gmail.com>
2024-07-27 10:36:31 +0530

    1

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Button } from "@mui/material";
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-red-600">My React App</h1>
+      <Button>Click Me</Button> 
+    </div>
+  );
+};
+
+export default App;
