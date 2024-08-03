d3e3f21eafb85212efd386aab1ca6ad23fc07dd5
vimal004 <2004.vimal@gmail.com>
2024-08-03 10:05:02 +0530

    7

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter, Outlet } from "react-router-dom";
 import Header from "./Header";
-import Home from "./Home";  
+import Home from "./Home";
 import About from "./About";
 
 const App = () => {
--- src/Home.jsx
+++ src/Home.jsx
@@ -3,6 +3,9 @@ const Home = () => {
     <div>
       <h1>Home</h1>
       <p>Welcome to the home page!</p>
+      <p className="hidden md:block lg:text-red-500">
+        Click on the "About" link in the header to visit the about page.
+      </p>
     </div>
   );
 };
