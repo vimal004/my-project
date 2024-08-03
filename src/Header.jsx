7f93829bdb1a5de47fa04878bb5020326a9e33a1
vimal004 <2004.vimal@gmail.com>
2024-08-03 10:10:35 +0530

    9

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,7 +3,7 @@ import { LoginSharp } from "@mui/icons-material";
 const Header = () => {
   return (
     <div className="py-2 border border-black text-center flex justify-center items-center space-x-4">
-      <img src="./src/assets/L.jpg" className="w-20" alt="Logo" />
+      <img src="public/L.jpg" className="w-20" alt="Logo" />
       <h1 className="">Siddha Shivalayas Patient Management</h1>
       <LoginSharp />
     </div>
