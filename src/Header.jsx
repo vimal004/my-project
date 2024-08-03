66367485e577fd1e1d57194eee09ea3943281cf7
vimal004 <2004.vimal@gmail.com>
2024-08-03 10:07:09 +0530

    8

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,14 +1,11 @@
 import { LoginSharp } from "@mui/icons-material";
 
-LoginSharp
 const Header = () => {
   return (
     <div className="py-2 border border-black text-center flex justify-center items-center space-x-4">
-      <img src="./src/assets/L.jpg"     className="w-28" alt="Logo" />
+      <img src="./src/assets/L.jpg" className="w-20" alt="Logo" />
       <h1 className="">Siddha Shivalayas Patient Management</h1>
-      <LoginSharp onClick={() => {
-        console.log("Login Button Clicked");
-      }} />
+      <LoginSharp />
     </div>
   );
 };
