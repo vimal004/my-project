e105c77f61716d4a301844a45fd5c56a1fcad5b0
vimal004 <2004.vimal@gmail.com>
2024-08-03 10:20:42 +0530

    9

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,12 +1,37 @@
+import React from "react";
 import { LoginSharp } from "@mui/icons-material";
+import { AppBar, Toolbar, Typography, IconButton, Box } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div className="py-2 border border-black text-center flex justify-center items-center space-x-4">
-      <img src="public/L.jpg" className="w-20" alt="Logo" />
-      <h1 className="">Siddha Shivalayas Patient Management</h1>
-      <LoginSharp />
-    </div>
+    <AppBar position="static">
+      <Toolbar>
+        <Box
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          sx={{ flexGrow: 1 }}
+        >
+          <img
+            src="public/L.jpg"
+            alt="Logo"
+            style={{
+              width: "38px",
+              height: "38px",
+              borderRadius: "50%",
+              marginRight: "20px",
+            }}
+          />
+          <Typography variant="h6" component="div">
+            Siddha Shivalayas Patient Management
+          </Typography>
+        </Box>
+        <IconButton component={Link} to="/login" color="inherit">
+          <LoginSharp />
+        </IconButton>
+      </Toolbar>
+    </AppBar>
   );
 };
 
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,65 @@
+import React from "react";
+import { Button, Stack } from "@mui/material";
+import { Link } from "react-router-dom";
+
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
-      <p>Welcome to the home page!</p>
-      <p className="hidden md:block lg:text-red-500">
-        Click on the "About" link in the header to visit the about page.
-      </p>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        backgroundColor: "#f0f2f5",
+      }}
+    >
+      <Stack spacing={2}>
+        <Button
+          component={Link}
+          to="/addpatient"
+          variant="contained"
+          color="primary"
+          size="large"
+        >
+          Manage Patient Record
+        </Button>
+        <Button
+          component={Link}
+          to="/billing"
+          variant="contained"
+          color="secondary"
+          size="large"
+        >
+          Billing
+        </Button>
+        <Button
+          component={Link}
+          to="/productpurchases"
+          variant="contained"
+          color="success"
+          size="large"
+        >
+          Product Purchases
+        </Button>
+        <Button
+          component={Link}
+          to="/appointments"
+          variant="contained"
+          color="info"
+          size="large"
+        >
+          Appointments
+        </Button>
+        <Button
+          component={Link}
+          to="/productstocks"
+          variant="contained"
+          color="warning"
+          size="large"
+        >
+          Product Stocks
+        </Button>
+      </Stack>
     </div>
   );
 };
